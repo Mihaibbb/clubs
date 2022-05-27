d29a00658e0ab63cc7311cd3a8c2867ac7863781
Mihaibbb <mihaibbb10@gmail.com>
2022-05-28 01:29:57 +0300

    reparare 2

---
+++ clubs-app/src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css';
+import {useState} from "react";
+import React from 'react'
+
+import {
+  BrowserRouter, 
+  Routes,
+  Route
+} from 'react-router-dom';
+
+import Home from './Components/Home';
+import AboutUs from './Components/AboutUs';
+import ContactUs from './Components/ContactUs';
+function App() {
+  
+  return (
+  
+      <BrowserRouter>
+          <Routes> 
+            <Route path="/" element={<Home />} />
+          </Routes>
+      </BrowserRouter>
+    
+  );
+}
+
+export default App;
